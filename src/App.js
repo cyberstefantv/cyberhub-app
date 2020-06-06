358f4fd682ac51dfb70920e4cdc87439ba99dabf
CyberStefan <hi@cyberstefan.com>
2020-06-06 18:17:02 -0400

    feat: fetch all cyberstefantv repos in list with apollo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { Button, Alert } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import { HomeScreen } from './HomeScreen';
 
 const Stack = createStackNavigator();
 
+let token = '';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
+  uri: 'https://api.github.com/graphql',
+});
+
+const showSetTokenAlert = () => {
+  Alert.prompt(
+    'Set Token',
+    'Enter a GitHub API token so that you can use the app.',
+    (text) => {
+      token = text;
+    },
+  );
+};
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerLeft: () => (
+                <Button onPress={showSetTokenAlert} title="Set Token" />
+              ),
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 };
 
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,27 +1,91 @@
-import React from 'react';
+import React, { useLayoutEffect, useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
+  FlatList,
   Text,
-  StatusBar,
+  View,
+  Button,
 } from 'react-native';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
-export const HomeScreen = () => {
+// Use https://developer.github.com/v4/explorer/
+// to quickly figure out queries.
+const REPOS = gql`
+  {
+    organization(login: "cyberstefantv") {
+      repositories(first: 20) {
+        nodes {
+          id
+          name
+          nameWithOwner
+          url
+        }
+      }
+    }
+  }
+`;
+
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
+
+export const HomeScreen = ({ navigation }) => {
+  const [fetchRepos, { called, loading, error, data }] = useLazyQuery(REPOS);
+
+  useEffect(() => {
+    fetchRepos();
+  }, [fetchRepos]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button
+          onPress={() => {
+            fetchRepos();
+          }}
+          title="Reload"
+        />
+      ),
+    });
+  }, [navigation, fetchRepos]);
+
+  if (!called || loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>{error.message}</Text>;
+  }
+
+  console.log(data);
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Text>Hello, World</Text>
-        </ScrollView>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={data.organization.repositories.nodes}
+          renderItem={({ item }) => <Item title={item.name} />}
+        />
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   scrollView: {},
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
 });
